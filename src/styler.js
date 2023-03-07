727d75bdd924cdbdec19fa9c2aa1064c023df8d2
Rohan Singh <vicky94rohan@gmail.com>
2023-03-07 01:29:45 -0800

    HTML and JS files added

---
+++ src/styler.js
@@ -0,0 +1,19 @@
+function toggleNightMode() {
+  const style = document.getElementById('style-link');
+
+  if (style.getAttribute('href') == '../src/style-light.css') {
+    style.setAttribute('href', '../src/style-dark.css');
+  } else {
+    style.setAttribute('href', '../src/style-light.css');
+  }
+
+  const images = document.getElementsByTagName('img');
+  for (const image of images) {
+    const src = image.getAttribute('src');
+    if (src.includes('img/light-mode')) {
+      image.setAttribute('src', src.replace('/light-mode/', '/dark-mode/'));
+    } else if (src.includes('img/dark-mode')) {
+      image.setAttribute('src', src.replace('/dark-mode/', '/light-mode/'));
+    }
+  }
+}
